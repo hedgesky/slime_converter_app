2cb2f2f439aff26e733cc8b77006a4d48e44fe66
Anton Chuchkalov <a2new@yandex.ru>
2017-01-29 12:03:19 +0300

    signal about error

--- web/static/js/init_textareas.js
+++ web/static/js/init_textareas.js
@@ -1,15 +1,15 @@
-var input_mirror, output_mirror, last_error;
+var input_mirror, output_mirror, last_error, $output_mirror_el;
 var toast_duration = 2000;
 
 function sendRequest() {
-  var params = { slime: input_mirror.getValue() }
-  $.post("/api/slime2html", params)
+  $.post("/api/slime2html", { slime: input_mirror.getValue() })
     .done(function(response) {
       output_mirror.setValue(response.html)
       if (last_error) {
         Materialize.toast("Now it's OK", toast_duration, "green lighten-1")
         last_error = null;
       };
+      $output_mirror_el.removeClass("error")
     })
     .fail(function(response) {
       var message = response.responseJSON.error;
@@ -17,6 +17,7 @@ function sendRequest() {
         Materialize.toast(message, toast_duration, "red lighten-1")
         last_error = message;
       }
+      $output_mirror_el.addClass("error")
     })
 }
 
@@ -34,6 +35,8 @@ output_mirror = CodeMirror.fromTextArea($("#html-output").get()[0], {
   mode: "application/x-erb",
   readOnly: true
 })
+
+$output_mirror_el = $(output_mirror.getWrapperElement())
 
 input_mirror.on("change", function(){
   sendRequest()
