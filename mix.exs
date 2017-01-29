f9defeac47bdb6eb46800a8417165b3110bd576a
Anton Chuchkalov <a2new@yandex.ru>
2017-01-29 13:48:41 +0300

    use Elixir 1.3 to allow heroku deploy

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SlimeConverter.Mixfile do
   def project do
     [app: :slime2html,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
